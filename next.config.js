029e2f90835741bab53d0e9d240658acb84ce023
Leegunwoo <lgw7537@gmail.com>
2023-11-20 11:46:29 +0900

    1

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["gateway.ipfscdn.io"],
+    domains: [
+      'gateway.ipfscdn.io',
+      'bafybeifrkzq3x5zad362adety5c7spf6zv5edmjwbrjic4auyq25bsto5i.ipfs.cf-ipfs.com',
+    ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
